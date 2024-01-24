a86ae5fa587fea6975db87bbd542c68bdd77a835
tommy-pellerin <tommytp@hotmail.fr>
2024-01-24 16:21:40 +0100

    maj des methodes dans le fichier de conversation

--- lib/open_ai_conversation.rb
+++ lib/open_ai_conversation.rb
@@ -6,41 +6,37 @@ require 'pry'
 
 Dotenv.load # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
 
-
 conversation_history = []
+def history(conversation_history,prompt_or_answer_to_stock)
+  conversation_history << prompt_or_answer_to_stock.to_s
+  return conversation_history
+end
 
-def converse_with_ai(conversation_history)
-  # création de la clé d'api et indication de l'url utilisée.
-  api_key = ENV["OPENAI_API_KEY"]
-  # url = "https://api.openai.com/v1/models"
-  url = "https://api.openai.com/v1/completions"
 
-  # un peu de json pour faire la demande d'autorisation d'utilisation à l'api OpenAI
-  headers = {
-    "Content-Type" => "application/json",
-    "Authorization" => "Bearer #{api_key}"
-  }
+# création de la clé d'api et indication de l'url utilisée.
+api_key = ENV["OPENAI_API_KEY"]
+# url = "https://api.openai.com/v1/models"
+url = "https://api.openai.com/v1/completions"
 
+# un peu de json pour faire la demande d'autorisation d'utilisation à l'api OpenAI
+headers = {
+  "Content-Type" => "application/json",
+  "Authorization" => "Bearer #{api_key}"
+}
+
+def converse_with_ai(api_key,url,headers,conversation_history)
   # un peu de json pour envoyer des informations directement à l'API
   puts "Quest veux tu demander à chatGPT ?"
-
-  def history(response)
-
-  end
-
   loop do
     print "Entre 'q' pour arreter > "
     new_prompt = gets.chomp
     
     break if new_prompt == 'q'
-    conversation_history << new_prompt.to_s
+    # conversation_history = history(conversation_history,new_prompt)
 
-    prompt_to_send = conversation_history.join("\n")
+    prompt_to_send = history(conversation_history,new_prompt).join("\n")
     # binding.pry
-    
-    
-
-
+        
     data = {
       # "model" => "gpt-3.5-turbo-instruct",
       "model" => "gpt-3.5-turbo-instruct",
@@ -52,18 +48,16 @@ def converse_with_ai(conversation_history)
     }
 
     response = HTTP.post(url, headers: headers, body: data.to_json)
-
     response_body = JSON.parse(response.body.to_s)
-
     response_string = response_body['choices'][0]['text'].strip
 
     print "chatGPT : "
     puts response_string
-    
-    conversation_history << response_string
+    history(conversation_history,response_string)
+    # conversation_history << response_string
 
   end
 end
 
-converse_with_ai(conversation_history)
+converse_with_ai(api_key,url,headers,conversation_history)
 
